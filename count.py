ad3a0a4832a50c6b91cc350abcdf6ab0e4439d04
Aiko <amarjot@uwindsor.ca>
2026-08-10 23:55:44 -0400

    Learning Advanced Strings

---
+++ count.py
@@ -0,0 +1,10 @@
+text="Python Python Docker Python" #How many times does this text appear
+
+print(text.count("Python"))
+
+text = "Python Python Docker"
+
+print(text.find("Python"))
+print(text.find("Docker"))
+print(text.find("Java"))
+print(text.count("Python"))
