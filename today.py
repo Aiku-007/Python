2df6a3080d33072024e7e9b5e0fc3f850fa739fb
Aiko <amarjot@uwindsor.ca>
2026-08-11 23:10:33 -0400

    Learning Advanced Strings

---
+++ today.py
@@ -0,0 +1,8 @@
+username = "Aiko123"
+
+print(username.isalpha())
+print(username.isdigit())
+print(username.isalnum())
+print(username.isspace())
+print(username.islower())
+print(username.isupper())
