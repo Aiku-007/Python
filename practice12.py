2789565453bbd9a9d30d1052e3d5e640aae2997c
Aiko <amarjot@uwindsor.ca>
2026-08-12 23:50:13 -0400

    Practicing Advanced Strings

---
+++ practice12.py
@@ -0,0 +1,15 @@
+log = "  ERROR: Server connection failed  "
+
+print(log.strip())
+
+print(log.lower())
+
+print(log.startswith("ERROR"))
+
+print(log.strip().startswith("ERROR"))
+
+print(log.count("e"))
+
+print(log.find("Server"))
+
+print(log.replace("ERROR", "WARNING"))
