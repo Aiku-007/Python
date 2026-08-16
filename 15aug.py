a5b91ebe05b741a26d89bf58817f30e9942f0164
Aiko <amarjot@uwindsor.ca>
2026-08-15 23:37:36 -0400

    15 august practice

---
+++ 15aug.py
@@ -0,0 +1,12 @@
+text="hello world python"
+words=text.split()
+print(words)
+
+some=['hell','word','pyramid']
+txt=" ".join(some)
+print(next)
+
+servers=["web01","web02","web03"]
+
+result=",".join(servers)
+print(result)
