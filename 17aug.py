001573d617389f52211a2dbc527b3af8f29a6639
Aiko <amarjot@uwindsor.ca>
2026-08-17 23:47:22 -0400

    m

--- 17aug.py
+++ 17aug.py
@@ -2,3 +2,8 @@ filename = "backup.zip"
 
 if filename.endswith(".zip"):
     print("This is a backup file")
+
+filename_2 = "database_backup.sql"
+
+if filename_2.endswith(".sql"):
+    print("This is a sql file")
