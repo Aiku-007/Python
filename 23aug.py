17b3f812cb9708b2dd7077de7670f888fd15841a
Aiko <amarjot@uwindsor.ca>
2026-08-23 23:43:32 -0400

    Add server check script in 23aug.py

---
+++ 23aug.py
@@ -0,0 +1,8 @@
+servers=["Server1","server2"]
+print("Servers to check:")
+
+for server in servers:
+  print(f"Checking {server}..")
+  print(f"{server} is online")
+
+print("Server check completed!")
