466aaeb49ab47d1a29ea9cdfc1f68b61c063cdf7
Aiko <amarjot@uwindsor.ca>
2026-08-26 23:55:15 -0400

    Mastering Advanced Strings

---
+++ adv_str.py
@@ -0,0 +1,16 @@
+text="Devops"
+print(text[0])
+print(text[::-1])
+print(text[1:3:1])
+#strings are immutable
+word="Python"
+modified_word=word.upper()
+print(modified_word)
+lower_word=word.lower()
+print(lower_word)
+
+username="   Aiko   "
+h=username.strip()
+
+print(h)
+
