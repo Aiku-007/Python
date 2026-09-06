c519e9d61ee530568067faccb62beefe3f6e6e28
Aiko <amarjot@uwindsor.ca>
2026-09-05 23:32:43 -0400

    How to find greatest number in list

--- greater.py
+++ greater.py
@@ -1,2 +1,10 @@
 numbers = [4, 12, 7, 20, 3, 15, 8]
-#Find the largest number that is greater than 10.
+
+largest = None
+
+for number in numbers:
+    if number > 10:
+        if largest is None or number > largest:
+            largest = number
+
+print("The largest number greater than 10 is:", largest)
