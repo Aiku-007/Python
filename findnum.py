bb647aee2ef992dfb395279a93d6590953c4e155
Aiko <amarjot@uwindsor.ca>
2026-09-06 23:47:35 -0400

    Practicing DSA and problem solving

---
+++ findnum.py
@@ -0,0 +1,6 @@
+numbers = [4, 8, 12, 20, 25, 30]
+
+for number in numbers:
+    if number==20:
+        print("Found it!")
+        break
---
+++ firstgreat.py
@@ -0,0 +1,6 @@
+numbers = [3, 7, 4, 15, 2, 20, 9]
+
+for number in numbers:
+    if number>10:
+        print("The first number greater than 10 is:", number)
+        break
