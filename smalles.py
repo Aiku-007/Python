bb52b00326582b74f3123b5c72313dffd3a8ddba
Aiko <amarjot@uwindsor.ca>
2026-09-10 23:31:07 -0400

    Practicing DSA

---
+++ smalles.py
@@ -0,0 +1,9 @@
+numbers = [4, 17, 8, 23, 12, 6, 19]
+
+for number in numbers:
+    if number > 10:
+        if number < smallest_number:
+         smallest_number=number
+
+
+print("The smallest number greater than 10 is ",smallest_number)
